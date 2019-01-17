adc94dc0c50b03b5bd899c0df091c71896198b5b
Oliver Bath <oliverbth05@gmail.com>
2019-01-17 00:20:48 +0000

    forum v2.1 release

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get('/*', (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
 })
 
-app.listen(process.env.PORT, process.env.IP, () => {
+app.listen(process.env.PORT || 3000, process.env.IP, () => {
     console.log('listening');
 })  
